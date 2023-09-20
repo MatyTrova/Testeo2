dd67796383704e57131b3d9267400b7021cc5fd5
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-09-20 14:32:49 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -332,22 +332,13 @@ with colA :
         c.drawString(100, 540, "Liquidación de pago")   
             
         c.setFont("Helvetica", 12)   
-        c.drawString(200, 540, f"Monto actual: ${lista_variables[0]}")
-        c.drawString(200, 520, f"Monto a cobrar: {lista_variables[1]}")
-        c.drawString(200, 500, f"Total de descuentos: {lista_variables[11]}%")
-        c.drawString(200, 480, f"Total de descuentos en pesos: ${lista_variables[2]}")
-        c.drawString(200, 460, f"Neto a percibir: ${lista_variables[3]}")
+     
         
         c.setFont("Helvetica-Bold", 12)
         c.drawString(200, 380, f"Detalle de descuentos")
             
         c.setFont("Helvetica", 12)
-        c.drawString(200, 360, f"Tasa del programa {programa_seleccionado} ({tasas_a_STR}%): ${lista_variables[4]}")
-        c.drawString(200, 340, f"IVA (10,5%) ley 25.063: ${lista_variables[7]}")
-        c.drawString(200, 320, f"II.BB para {provincia_seleccionada} (Alícuota Gral.)({porcentaje_iibb_str}%): ${lista_variables[8]}")
-        c.drawString(200, 300, f"IVA RG2408 (1,5%): ${lista_variables[9]}")
-        c.drawString(200, 280, f"Arancel T.Cred (1,8%): ${lista_variables[5]}")
-        c.drawString(200, 260, f"IVA (21%): ${lista_variables[6]}")
+
         
         if (tipo_inscripcion != "Monotributista"):
             c.drawString(40, 220, f"Al estar inscripto como {tipo_inscripcion} usted recuperará ${lista_variables[10]} en concepto de IVA")
