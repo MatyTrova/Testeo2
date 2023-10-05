a1faef57acf709dd3b71d6f5d06f8007106cd7a6
Matias Trovatto <118058206+MatyTrova@users.noreply.github.com>
2023-10-05 15:29:13 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -956,6 +956,6 @@ custom_css = """
             }
         </style>
         """
-ola = f'<div class="ag-format-container"><div class="ag-courses_box"><div class="ag-courses_item_core_2"><div class="ag-courses-item_link_core_3"><div class="ag-courses-item_title_core">Interesados en la oferta de la semana</div><img src="data:image/png;base64,{imagen_codificada2}" alt="Gráfico de Pastel"></div></div></div></div>'
+ola = f'<div class="ag-format-container"><div class="ag-courses_box"><div class="ag-courses_item_core_2"><div class="ag-courses-item_link_core_3"><div class="ag-courses-item_title_core">Interesados en la oferta de la semana</div><img src="data:image/png;base64" alt="Gráfico de Pastel"></div></div></div></div>'
 
 st.markdown(ola, unsafe_allow_html=True)
